113125d357ef49a8cde1f27486319347a2462d38
Edwin Sutanudjaja <edwinkost@UU109053>
2022-09-19 10:25:37 +0200

    make subdomains at 30sec

--- develop/make_clone_and_landmask_maps_30sec_develop.py
+++ develop/make_clone_and_landmask_maps_30sec_develop.py
@@ -136,7 +136,7 @@ def main():
     catchmenttotal = pcr.catchmenttotal(pcr.scalar(1.0), ldd_map)
     outlets = pcr.pit(ldd_map)
     outlets_boolean = pcr.defined(outlets)
-    outlets_ordered = pcr.arearank(pcr.ifthen(outlets_boolean, catchmenttotal) * -1.0, pcr.nominal(outlets_boolean))
+    outlets_ordered = pcr.areaorder(pcr.ifthen(outlets_boolean, catchmenttotal) * -1.0, pcr.nominal(outlets_boolean))
     catchment_map = pcr.areamajority(outlets_ordered, catchment_map)
     
 
